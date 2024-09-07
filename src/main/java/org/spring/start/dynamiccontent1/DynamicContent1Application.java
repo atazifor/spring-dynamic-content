17a43d30a985d40ebc1ecf7da09418e89a66324a
Amin Tazifor <amin.tazifor@gmail.com>
2024-09-07 20:46:32 +0100

    first attempt at dynamic content to a thymeleaf template

---
+++ src/main/java/org/spring/start/dynamiccontent1/DynamicContent1Application.java
@@ -0,0 +1,13 @@
+package org.spring.start.dynamiccontent1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicContent1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicContent1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/start/dynamiccontent1/DynamicContent1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.start.dynamiccontent1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicContent1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
