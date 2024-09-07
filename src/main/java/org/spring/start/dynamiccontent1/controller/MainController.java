732c0fefdcc0f68807e0736be54a5222b2a40f21
Amin Tazifor <amin.tazifor@gmail.com>
2024-09-07 21:07:54 +0100

    send data from client to server using path  variable

--- src/main/java/org/spring/start/dynamiccontent1/controller/MainController.java
+++ src/main/java/org/spring/start/dynamiccontent1/controller/MainController.java
@@ -2,17 +2,17 @@ package org.spring.start.dynamiccontent1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
-    public String home(@RequestParam(required = false) String color,
-                       @RequestParam(required = false) String  username,
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color,
                        Model page) {
-        page.addAttribute("username", username);
+        page.addAttribute("username", "Amin");
         page.addAttribute("color", color);
         return "home.html";
     }
